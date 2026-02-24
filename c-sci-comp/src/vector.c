8b7c6fc2363708fca919d1014d32bacc3b103959
Premanshu Bhagat <premtsapj@gmail.com>
2026-02-23 19:10:25 -0500

    added vector creation method

---
+++ c-sci-comp/src/vector.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "vector.h"
+
+vector_t* create_vec(size_t size)
+{
+    vector_t* vec = malloc(sizeof(vector_t));
+    if (!vec)
+    {
+        return NULL;
+    }
+    vec->size = size;
+    vec->data = calloc(size, sizeof(double));
+
+    if (!vec->data)
+    {
+        free(vec);
+        return NULL;
+    }
+    return vec;
+}
